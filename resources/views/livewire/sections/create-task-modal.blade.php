7c2d94e3ad2227f26644dd4c6f29a3d989248992
jerome-shiftleft <jerome@theshiftleft.com>
2023-10-26 05:36:47 +0800

    code cleanup

--- resources/views/livewire/sections/create-task-modal.blade.php
+++ resources/views/livewire/sections/create-task-modal.blade.php
@@ -1,14 +1,9 @@
 <div id="create-task-modal" class="task-modal">
   <form id="create-task-form">
-
     <h2 class="modal-title">Create Task</h2>
-
-    <div class="validation-error">
-
-    </div><!-- /#validation-error -->
+    <div class="validation-error"></div>
 
     <div class="form-inputs">
-
       <select name="task_project_id" id="task-project-id" class="form-select" required>
         <option value="">Select Project</option>
         @foreach ($projects as $project)
@@ -17,15 +12,12 @@
       </select>
 
       <input type="text" id="title" name="title" class="form-control" placeholder="Title" required>
-
       <textarea id="description" name="description" class="form-control" rows="4" placeholder="Description"></textarea>
-
     </div><!-- /.form-inputs -->
 
     <div class="modal-actions">
       <button type="button" id="create-task-cancel-btn" class="btn btn-secondary">Cancel</button>
       <button id="create-task-btn" type="submit" class="btn btn-primary">Create</button>
     </div><!-- /.modal-actions -->
-
   </form>
 </div><!-- /#create-task-modal -->
--- resources/views/livewire/sections/update-task-modal.blade.php
+++ resources/views/livewire/sections/update-task-modal.blade.php
@@ -3,14 +3,10 @@
 
     <h2 class="modal-title">Update Task</h2>
 
-    <div class="validation-error">
-
-    </div><!-- /#validation-error -->
+    <div class="validation-error"></div>
 
     <div class="form-inputs">
-
       <input type="hidden" id="update-task-id">
-
       <select name="update_project_id" id="update-project-id" class="form-select" required>
         <option value="">Select Project</option>
         @foreach ($projects as $project)
@@ -19,15 +15,12 @@
       </select>
 
       <input type="text" id="update-title" name="title" class="form-control" placeholder="Title" required>
-
       <textarea id="update-description" name="description" class="form-control" rows="4" placeholder="Description"></textarea>
-
     </div><!-- /.form-inputs -->
 
     <div class="modal-actions">
       <button type="button" id="update-task-cancel-btn" class="btn btn-secondary">Cancel</button>
       <button id="update-task-btn" type="submit" class="btn btn-primary">Save</button>
     </div><!-- /.modal-actions -->
-
   </form>
 </div><!-- /#update-task-modal -->
